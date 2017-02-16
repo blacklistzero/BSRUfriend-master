c187c8a336ebd1a7fb75636cbf602e690b4ecf6b
blacklistzero <njjghtdrg@gmail.com>
2017-02-16 11:51:11 +0700

    checkuser pass ok

--- app/src/main/java/appewtc/masterung/bsrufriend/MainActivity.java
+++ app/src/main/java/appewtc/masterung/bsrufriend/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -98,9 +99,17 @@ public class MainActivity extends AppCompatActivity {
             } //for
 
             if (aBoolean) {
-                    //User False
+                //User False
                 MyAlert myAlert = new MyAlert(MainActivity.this);
                 myAlert.myDialog("หา User ไม่เจอ", "ไม่มี" + userString + "ในฐานข้อมูล");
+            } else if (!passString.equals(loginStrings[3])) {
+                // password false
+                MyAlert myAlert = new MyAlert(MainActivity.this);
+                myAlert.myDialog("Password ผิด", "กรุณาใส่รหัสใหม่");
+            } else {
+                //password true
+                Toast.makeText(MainActivity.this, "ยินดีต้อนรับ" + loginStrings[1],
+                        Toast.LENGTH_SHORT).show();
             }
 
 
