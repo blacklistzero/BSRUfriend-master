bcdbdd6d45cebe3015ccf986cf5b05f08827b75b
blacklistzero <njjghtdrg@gmail.com>
2017-02-17 12:00:38 +0700

    17.02.60 map

--- app/src/main/java/appewtc/masterung/bsrufriend/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/bsrufriend/ServiceActivity.java
@@ -19,8 +19,12 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ServiceActivity extends FragmentActivity implements OnMapReadyCallback {
     //explicit
@@ -70,7 +74,7 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         //doing
         afterResume();
         updateLatLng();
-
+        createMarker();
 
         //delay
         if (aBoolean) {
@@ -86,6 +90,35 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
 
     }// myloop
 
+    private void createMarker() {
+        try {
+            mMap.clear();
+            String urlPHP = "http://swiftcodingthai.com/bsru/get_user_master.php";
+            int[] avataInts = new int[]{R.drawable.bird48, R.drawable.doremon48,
+                    R.drawable.kon48, R.drawable.nobita48, R.drawable.rat48};
+
+            GetUser getUser = new GetUser(ServiceActivity.this);
+            getUser.execute(urlPHP);
+            String strJSON = getUser.get();
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+            for (int i=0;i<jsonArray.length();i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                LatLng latLng = new LatLng(Double.parseDouble(jsonObject.getString("Lat")),
+                        Double.parseDouble(jsonObject.getString("Lng")));
+                mMap.addMarker(new MarkerOptions().position(latLng)
+                .icon(BitmapDescriptorFactory
+                        .fromResource(avataInts[Integer.parseInt(jsonObject.getString("Avata"))]))
+                .title(jsonObject.getString("Name")));
+
+            } //for
+            getUser.cancel(true);
+        } catch (Exception e) {
+            Log.d("17febV3", "e createMarker ==> " + e.toString());
+        }
+
+    }//Create Marker
+
     private void updateLatLng() {
         try {
             EditLatLng editLatLng = new EditLatLng(ServiceActivity.this, loginStrings[0]);
@@ -93,6 +126,9 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
                     Double.toString(userLngADouble));
             boolean b = Boolean.parseBoolean(editLatLng.get());
             Log.d("17febV2", "Result ==> " + b);
+            editLatLng.cancel(true);
+
+
 
         } catch (Exception e) {
             Log.d("17febV2", "e update ==>" + e.toString());
@@ -144,7 +180,6 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
             return;
         }
         locationManager.removeUpdates(locationListener);
-
 
     }
 
